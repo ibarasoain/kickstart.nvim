5bdb5034dcac76ea3dbc4a852bd9882ed3597037
ibarasoain <ibarasoaine@unav.es>
2025-09-18 11:48:14 +0200

    Updated vimtex config

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -5,5 +5,21 @@ return {
   init = function()
     -- VimTeX configuration goes here, e.g.
     vim.g.vimtex_view_method = 'zathura'
+    -- vim.g.vimtex_compiler_latexmk = {
+    --   aux_dir = 'aux', -- create a directory called aux that will contain all the auxiliary files
+    -- }
+    vim.g.vimtex_compiler_latexmk = {
+      aux_dir = '',
+      out_dir = 'output',
+      callback = 1,
+      continuous = 1,
+      executable = 'latexmk',
+      options = {
+        '-verbose',
+        '-file-line-error',
+        '-synctex=1',
+        '-interaction=nonstopmode',
+      },
+    }
   end,
 }
